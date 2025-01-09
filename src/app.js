27819d4842d3d980ec648daaff70f310773571a2
merine <ambre92000@hotmail.com>
2025-01-09 23:08:12 +0100

    modif app.js pour les routes nosql

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');   // Importe le module express
 const bodyParser = require('body-parser');   // Importe le module body-parser
 const taskRoutes = require('./taskRoutes');    // Importe le fichier taskRoutes.js
+const userRoutesNoSQL = require('./userRoutesNoSQL');
 
 const app = express();    // Crée une instance d'application Express
 const port = 3000;    // Port sur lequel ton serveur va écouter
 
 app.use(bodyParser.json()); // Pour parser les requêtes JSON
 app.use('/api', taskRoutes); // Préfixe des routes API
+app.use('/api', userRoutesNoSQL);
 
 app.listen(port, () => {     // Démarre le serveur
   console.log(`Serveur démarré sur http://localhost:${port}`);
 });
+
